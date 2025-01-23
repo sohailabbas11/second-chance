0033068cf097f4386df41635d7e71b1529ba5750
sohail abbas <sohailabbas669@gmail.com>
2025-01-23 21:07:53 +0100

    deleted all

--- matlib.py
+++
@@ -1,9 +0,0 @@
-adjective = input('adjective: ')
-verb1 = input("verb1: ")
-verb2 = input('verb2: ')
-famous_person = input('famous_person:')
-
-matlib = f'i like {adjective}, and always want to {verb1} to {verb2} ,so \
-i look like {famous_person}'
-
-print(matlib)
