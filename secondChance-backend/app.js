307268539c86e12eba0482e4a242de820bec6129
sohail abbas <sohailabbas669@gmail.com>
2025-01-24 12:36:50 +0100

    setting up mongodb and populatin data

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -6,6 +6,7 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 const app = express();
 app.use("*",cors());
@@ -20,6 +21,7 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 //{{insert code here}}
