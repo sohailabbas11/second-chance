8e3ed5b2107a32de2d3104425eee02ff62264268
sohail abbas <sohailabbas669@gmail.com>
2025-01-25 13:12:41 +0100

    folder created

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,6 +1,5 @@
-// db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,23 +8,13 @@ let dbInstance = null;
 const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
-
-    const client = new MongoClient(url);      
-
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-    await client.connect()
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-     dbInstance = client.db(dbName)
-
-    // Task 3: Return database instance
-    // {{insert code}}
-    return dbInstance
+  if (dbInstance) {
+    return dbInstance;
+  }
+  const client = new MongoClient(url);
+  await client.connect();
+  dbInstance = client.db(dbName);
+  return dbInstance;
 }
 
 module.exports = connectToDatabase;
